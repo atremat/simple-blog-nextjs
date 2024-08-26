324afc80d7a055087d80c8a21caa0a062ce7dbc8
Artem <atremat@gmail.com>
2024-08-27 01:18:33 +0300

    back to useEffect

--- src/app/ArticlePreview.tsx
+++ src/app/ArticlePreview.tsx
@@ -3,25 +3,27 @@
 import { useEffect, useState } from 'react';
 import { ROUTING } from './routing';
 import AppLink from './shared/components/app-link';
-import { articleStorage } from './ArticleStorage';
 
 type ArticlePreviewProps = {
   name: string;
   text: string;
 };
 
-export function ArticlePreview({ name, text }: ArticlePreviewProps) {
-  const [liked, setLiked] = useState(articleStorage.liked(name));
+const getLikeKey = (articleName: string) => `developer_blog_${articleName}`;
 
-  // useEffect(() => {
-  //   const likeKey = getLikeKey(name);
-  //   const likeValueString = localStorage.getItem(likeKey);
-  //   const likeValue = likeValueString ? JSON.parse(likeValueString) : false;
-  //   setLiked(likeValue === true);
-  // }, [name]);
+export function ArticlePreview({ name, text }: ArticlePreviewProps) {
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    const likeKey = getLikeKey(name);
+    const likeValueString = localStorage.getItem(likeKey);
+    const likeValue = likeValueString ? JSON.parse(likeValueString) : false;
+    setLiked(likeValue === true);
+  }, [name]);
 
   const like = () => {
-    articleStorage.like(name);
+    const likeKey = getLikeKey(name);
+    localStorage.setItem(likeKey, JSON.stringify(!liked));
     setLiked(!liked);
   };
 
